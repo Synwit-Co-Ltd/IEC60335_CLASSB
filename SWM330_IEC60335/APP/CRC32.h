f27453c731f6d1fce24a7e2c8e294ff4b1bb7c97
mowsong <mowsong@gmail.com>
2026-02-09 02:32:25 +0800

    remove SWM330 version based on SWM341

--- SWM330_IEC60335/APP/CRC32.h
+++
@@ -1,5 +0,0 @@
-#include <inttypes.h>
-
-extern uint32_t crc32_fsl(uint32_t crc, uint8_t *buf, uint32_t len);
-extern uint32_t crc32_fsl_continuous(uint32_t crc, uint8_t *buf, uint32_t len,uint8_t continue_flag);
-extern uint32_t CRC_CalcBlockCrc(const uint8_t *data, int len, uint32_t crc);
